c9fa8237b2d202a9f14fd7ecdd0cb96540fd7fea
mrm8488 <mrm8488@gmail.com>
2019-12-09 23:32:58 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const app = document.getElementById("app");
+
+const typewriter = new Typewriter(app, {
+  loop: false,
+  delay: 75,
+  autoStart: true,
+  cursor: "█",
+  strings: []
+});
+
+typewriter
+.typeString('We are Cybersecurity')
+.pauseFor(500)
+.deleteChars(13)
+  .typeString(
+    "<strong>CybersecuritAI</strong>: a game changer approach in the cybersecurity field."
+  )
+  .pauseFor(1000)
+   .deleteAll()
+  .typeString("<br /><br />")
+  .typeString(
+    "We use last <strong>Machine Learning and Deep Learning</strong> techniques to protect your <strong>web/app/API.</strong>"
+  )
+  .pauseFor(1000)
+  .typeString("<br /><br />")
+  //.deleteAll(1)
+  .typeString("Find us online: <br />")
+  .pauseFor(300)
+  .typeString('<a href="https://twitter.com/AiCybersecurit">Twitter</a> <br />')
+  .pauseFor(1000)
+  .typeString("<br /><br />")
+  //.deleteAll(1)
+  .typeString("More info: coming soon! <br />")
+  //..typeString('<a href="https://www.tiffanyrwhite.com">Portfolio</a> <br />');
